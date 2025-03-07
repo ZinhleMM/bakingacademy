89e883ef4e2f5bdf93256e870c0bdd830322c73d
Zinhle Maurice-Mopp <98168717+ZinhleMM@users.noreply.github.com>
2025-03-07 18:53:25 +0200

    Update urls.py

--- bakingacademy/urls.py
+++ bakingacademy/urls.py
@@ -1,6 +1,6 @@
 
 """
-URL configuration for project.
+URL configuration for bakingacademy project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
