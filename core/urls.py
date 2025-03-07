067136a97b9dc4b9e0115285f214a0a2bbe80e1f
Zinhle Maurice-Mopp <98168717+ZinhleMM@users.noreply.github.com>
2025-03-07 19:13:17 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views.user_views import register_user, login_user, logout_user
+from core.views.user_views import home, register_user, login_user, logout_user
+
+app_name = 'core'
 
 urlpatterns = [
+    path('', home, name='home'),  # Map the root URL to the home view
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
