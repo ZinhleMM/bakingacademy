136d4d953d5c538cb0f09b147b2068b211692a15
Zinhle Maurice-Mopp <98168717+ZinhleMM@users.noreply.github.com>
2025-03-07 17:13:41 +0200

    Update wsgi.py

--- bakingacademy/wsgi.py
+++ bakingacademy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bakingacademy.settings')
 
 application = get_wsgi_application()
